7c1cc0ba81aa8eba8a4aa4aea7e9f0363bfb1468
Komal <divyavairagade@gmail.com>
2022-05-10 01:13:04 +0530

    Program for stack uploaded

---
+++ RFP_DataStructure1/LinkedListStack.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RFP_DataStructure1
+{
+    class LinkedListStack
+    {
+        private Node top;
+        public LinkedListStack()
+        {
+            this.top = null;
+        }
+
+        internal void Push(int value)
+        {
+            Node node = new Node(value);
+            if (this.top == null)
+                node.next = null;
+            else
+                node.next = this.top;
+            this.top = node;
+            Console.WriteLine("{0} pushed to stack ", value);
+        }
+        internal void Peek()
+        {
+            if (this.top == null)
+            {
+                Console.WriteLine("Stack is empty");
+                return;
+            }
+            Console.WriteLine("{0} is in the top of the stack ", this.top.data);
+        }
+    }
+}
--- RFP_DataStructure1/Program.cs
+++ RFP_DataStructure1/Program.cs
@@ -1,3 +1,11 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
+using RFP_DataStructure1;
+
 Console.WriteLine("Program for Stack and Queue");
+
+LinkedListStack linkedListStack = new LinkedListStack();
+linkedListStack.Push(70);
+linkedListStack.Push(30);
+linkedListStack.Push(56);
+linkedListStack.Peek();
